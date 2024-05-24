9393dc81366b5c25dede31482b66946bf5f2e6a2
taur <taur@mail.com>
2024-05-24 14:26:30 +0200

    updated translations in index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="tr" lang="tr">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<title>Db Diffs</title>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
@@ -14,8 +14,8 @@ $r=$_REQUEST;
 $dd=new dbDiffs();
 
 /**
- * veritabanı kullanıcı bilgileri
- * */
+ * database user information
+ */
 $dd->db->username='root';
 $dd->db->password='root';
 
